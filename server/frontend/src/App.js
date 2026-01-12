b7eb7d3a268f5f8163cc5253fd3f37e545f337ad
sm00th76 <khantwalbaladitya@gmail.com>
2026-01-12 20:05:45 +0530

    login/logout/register

--- server/frontend/src/App.js
+++ server/frontend/src/App.js
@@ -1,11 +1,24 @@
 import LoginPanel from "./components/Login/Login"
+import Register from "./components/Register/register"
+import Dealers from "./components/Dealers/Dealers"
+import Dealer from "./components/Dealers/Dealer"
+import PostReview from "./components/Dealers/PostReview"
+import Header from "./components/Header/Header"
 import { Routes, Route } from "react-router-dom";
 
 function App() {
   return (
-    <Routes>
-      <Route path="/login" element={<LoginPanel />} />
-    </Routes>
+    <>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Dealers />} />
+        <Route path="/dealers" element={<Dealers />} />
+        <Route path="/dealer/:dealer_id" element={<Dealer />} />
+        <Route path="/login" element={<LoginPanel />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/post-review/:dealer_id" element={<PostReview />} />
+      </Routes>
+    </>
   );
 }
 export default App;
